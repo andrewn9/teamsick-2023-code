9f400e6757e375db10e6e3e08476e7229b73761f
Luke Lawlor <lklawlor1@gmail.com>
2023-05-29 19:34:41 -0400

    unified style, organized similar statements, and added comments

--- Tele-Op/RoboCentricDrive.java
+++ Tele-Op/RoboCentricDrive.java
@@ -1,7 +1,7 @@
 /*
-*   Team Sick
-*   Holonomic drive program using gamepad
-*/
+ *   Team Sick
+ *   Holonomic drive program using gamepad
+ */
 
 package org.firstinspires.ftc.teamcode;
 
@@ -15,82 +15,85 @@ import com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior;
 
 @TeleOp(name="RoboCentricDrive")
 public class RoboCentricDrive extends OpMode {
-    DcMotor fL, fR, bL, bR;
-    DcMotor arm;
-    Servo gripper;
+    // Hardware
+        // Front/back left/right mecanum wheel motors
+        DcMotor fL, fR, bL, bR;
     
-    private final int armMin = 10;
-    private final int armMax = 1500;
+        DcMotor arm;
+        Servo gripper;
     
-    private double drivePower= 0.7;
-    private double armPower = 1.0;
+    // Minimum and maximum arm target positions
+    final int ARM_POS_MIN = 10;
+    final int ARM_POS_MAX = 1500;
+
+    // Value that the arm's position is changed by each loop() call
+    final int ARM_MOVE_FACTOR = 20;
+
+    final double ARM_POWER = 1.0;
+        
+    // Maximum drive motor power
+    final double DRIVE_POWER = 0.7;
     
     @Override
     public void init() {
+	// Map hardware
         fL = hardwareMap.dcMotor.get("fL");
-        bl = hardwareMap.dcMotor.get("bL");
+        bL = hardwareMap.dcMotor.get("bL");
         fR = hardwareMap.dcMotor.get("fR");
         bR = hardwareMap.dcMotor.get("bR");
         gripper = hardwareMap.servo.get("gripper");
-        
         arm = hardwareMap.dcMotor.get("arm");
-        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        arm.setTargetPosition(0);
-        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        arm.setPower(armPower);
-        
-        gripper.setPosition(0);
-        
+
+	// Configure hardware
         fR.setDirection(DcMotorSimple.Direction.REVERSE);
         bR.setDirection(DcMotorSimple.Direction.REVERSE);
-        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        
+        gripper.setPosition(0);
 
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        arm.setTargetPosition(0);
+        arm.setPower(ARM_POWER);
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     @Override
     public void loop() {
+	// Move mecanum wheels using the gamepad1 sticks
         double y = -gamepad1.left_stick_y;
         double x = gamepad1.left_stick_x*1.1;
         double rx = gamepad1.right_stick_x;
         double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
 
-        fL.setPower((y+x+rx)/denominator * drivePower);
-        bL.setPower((y-x+rx)/denominator * drivePower);
-        bR.setPower((y+x-rx)/denominator * drivePower);
-        fR.setPower((y-x-rx)/denominator * drivePower);
-    
-        
+        fL.setPower((y+x+rx)/denominator * DRIVE_POWER);
+        bL.setPower((y-x+rx)/denominator * DRIVE_POWER);
+        bR.setPower((y+x-rx)/denominator * DRIVE_POWER);
+        fR.setPower((y-x-rx)/denominator * DRIVE_POWER);
+
+	// Move the arm using the gamepad1 dpad
         double armPosition = arm.getCurrentPosition();
-        
         if(gamepad1.dpad_down)
-        {
-           arm.setTargetPosition((int)armPosition-20);
-        }
+	    armPosition -= ARM_MOVE_FACTOR;
         else if(gamepad1.dpad_up)
-        {
-           arm.setTargetPosition((int)armPosition+20);
-        }
+	    armPosition += ARM_MOVE_FACTOR;
+        arm.setTargetPosition(clamp(armPosition,ARM_POS_MIN,ARM_POS_MAX));
         
-        arm.setTargetPosition(clamp(arm.getTargetPosition(),armMin,armMax));
-        
+	// Open/close the gripper using the gamepad1 dpad
         if(gamepad1.dpad_left)
-        {
            gripper.setPosition(1);
-        }
         else if(gamepad1.dpad_right)
-        {
            gripper.setPosition(0);
-        }
         
+	// Telemetry
         telemetry.addData("Arm Position", armPosition);
         telemetry.update();
     }
     
     public int clamp(int val, int min, int max)
     {
-        if(val<=min)
+        if(val<min)
             return min;
-        if(val>=max)
+        if(val>max)
             return max;
         return val;
     }
